audreyt/pugs

commit 82f608599cf6787aa37c17c0c231e85290ecd42b

* revert the failed attempt at emulating readline   using System.Console.SimpleLineEditor, as it didn't   quite work for dduncan++ (and neither for me)
git-svn-id: file:///data/svn/pugs@8829 c213334d-75ef-0310-aa23-eaa082d1ae64

src/Pugs/Shell.hs
@@ -21,8 +21,6 @@ import Pugs.Internals
 
 #ifdef PUGS_HAVE_READLINE
 import qualified System.Console.Readline as Readline
-#else
-import qualified System.Console.SimpleLineEditor as Readline
 #endif
 
 data Command
@@ -67,11 +65,11 @@ parseCommandLine (':':'l':str)  = CmdLoad $ unwords (words str)
 parseCommandLine str            = CmdRun (RunOpts False False True) str
 
 initializeShell :: IO ()
-initializeShell
+initializeShell = do
+    hSetBuffering stdout NoBuffering
+    hSetBuffering stdin  NoBuffering
 #ifdef PUGS_HAVE_READLINE
-   = Readline.initialize
-#else
-   = Readline.initialise
+    Readline.initialize
 #endif
 
 readline :: String -> IO (Maybe String)
@@ -81,7 +79,9 @@ readline prompt = do
     Readline.setCatchSigwinch False
     Readline.readline prompt
 #else
-    Readline.getLineEdited prompt
+    putStr prompt
+    input <- getLine
+    return $ Just input
 #endif
 
 addHistory :: String -> IO ()

commit 41cbda0529d98cd55d0cd923f27a69651efcbfe7

r2186@darren-duncans-power-mac-g4:  darrenduncan | 2006-01-29 00:41:00 -0800  src/Pugs/Shell.hs : updated the expressing setting initializeShell so it only invokes NoBuffering when we're using readline, rather than doing it unconditionally; this fixes a bug seemingly introduced in r8829 ... as a result, the pugs shell now handles the backspace/delete key correctly
git-svn-id: file:///data/svn/pugs@8832 c213334d-75ef-0310-aa23-eaa082d1ae64

src/Pugs/Shell.hs
@@ -65,11 +65,13 @@ parseCommandLine (':':'l':str)  = CmdLoad $ unwords (words str)
 parseCommandLine str            = CmdRun (RunOpts False False True) str
 
 initializeShell :: IO ()
-initializeShell = do
-    hSetBuffering stdout NoBuffering
-    hSetBuffering stdin  NoBuffering
+initializeShell
 #ifdef PUGS_HAVE_READLINE
-    Readline.initialize
+    = do hSetBuffering stdout NoBuffering
+         hSetBuffering stdin  NoBuffering
+         return Readline.initialize
+#else
+    = return ()
 #endif
 
 readline :: String -> IO (Maybe String)