audreyt/pugs

commit aaffe60569d3165fe1f070a280df9291da5b65e9

pi() is now more like spec.  I'm trying to get it right, as a way of shaking down Prelude.pm and Prim.hs problems. Prim.hs - "pi" renamed "Pugs::Internals::pi".  Surprisingly, it no longer works in this form!  Now yields {obj:Code::Exp}. Prelude.pm - Added Math::Basic::pi, S29.  I would have liked to ::= Pugs::Internals::pi, but with that not working, a literal value is used. Prelude.pm - Added *pi(), for backwards compatibility.  This is non-spec. S29 says  use Math::Basic :constants  is needed to get pi.  That's next.  I would have liked to ::= Math::Basic::pi, but that wasn't working in the Prelude.
git-svn-id: file:///data/svn/pugs@13255 c213334d-75ef-0310-aa23-eaa082d1ae64

src/Pugs/Prim.hs
@@ -90,7 +90,7 @@ op0 "File::Spec::cwd" = const $ do
 op0 "File::Spec::tmpdir" = const $ do
     tmp <- guardIO getTemporaryDirectory
     return $ VStr tmp
-op0 "pi" = constMacro . Val $ VNum pi
+op0 "Pugs::Internals::pi" = constMacro . Val $ VNum pi
 op0 "self" = const $ expToEvalVal (_Var "&self")
 op0 "say" = const $ op1 "IO::say" (VHandle stdout)
 op0 "print" = const $ op1 "IO::print" (VHandle stdout)
@@ -1657,7 +1657,7 @@ initSyms = mapM primDecl syms
 \\n   Num       pre     cos     safe   (Num)\
 \\n   Num       pre     sin     safe   (Num)\
 \\n   Num       pre     tan     safe   (Num)\
-\\n   Any       pre     pi      safe,macro   ()\
+\\n   Any       pre     Pugs::Internals::pi      safe,macro   ()\
 \\n   Any       pre     self    safe,macro   ()\
 \\n   Bool      pre     nothing safe   ()\
 \\n   Num       pre     exp     safe   (Num, ?Num)\

src/perl6/Prelude.pm
@@ -532,6 +532,11 @@ class Num {
     }
 }
 
+sub Math::Basic::pi () is primitive is safe {3.141592653589793}
+# *pi is non-spec (S29);  Should require use Math::Basic :constants;
+sub pi() is primitive is builtin is safe {Math::Basic::pi}
+
+
 sub sprintf ($fmt, *@args) is primitive is builtin is safe {
     my $flen = $fmt.chars;
     my $fi = 0;

commit 3231984c599620bfa69a9717a0512dd2f1c6ad64

t/xx-uncategorized/pi_internals.t - Created. New test for Pugs::Internals::pi in Prim.hs, broken by r13255's renaming of pi from "pi" to "Pugs::Internals::pi".
git-svn-id: file:///data/svn/pugs@13307 c213334d-75ef-0310-aa23-eaa082d1ae64

t/xx-uncategorized/pi_internals.t
@@ -1,0 +1,20 @@
+use v6-alpha;
+
+use Test;
+
+=kwid
+
+The value of Pugs::Internals::pi is {obj:Code::Exp} rather than 3.14... .
+This was caused by renaming pi in Prim.hs from "pi" to "Pugs::Internals::pi".
+r13255.
+
+  % ./pugs
+  pugs> Pugs::Internals::pi
+  {obj:Code::Exp}
+  pugs> 
+
+=cut
+
+plan 1;
+
+eval_ok '3.14 < Pugs::Internals::pi() < 3.15','Pugs::Internals::pi works, bug fixed.';